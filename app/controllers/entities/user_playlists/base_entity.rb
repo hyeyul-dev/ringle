a8ae17f44590962083975799833f1b8aadb56626
hyeyul-kim <71623971+hyeyul-kim@users.noreply.github.com>
2022-06-27 11:23:48 +0900

    [Add] preload in user_playlist entities

--- app/controllers/entities/user_playlists/base_entity.rb
+++ app/controllers/entities/user_playlists/base_entity.rb
@@ -1,4 +1,8 @@
 class Entities::UserPlaylists::BaseEntity < Grape::Entity
   expose :id, documentation: { type: 'Integer', desc: '재생목록 id' }
   expose :music_user_playlists, using: Entities::MusicUserPlaylists::BaseEntity
+
+  def music_user_playlists
+    object.music_user_playlists.preload(music: :artists)
+  end
 end
